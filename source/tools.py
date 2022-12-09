4c494451a98a20a3aeb59584e2539bc72e1ef947
Harry-58 <56926733+Harry-58@users.noreply.github.com>
2022-12-09 07:47:08 +0100

    Erweitert füs SMS

--- source/tools.py
+++ source/tools.py
@@ -1,8 +1,8 @@
 from machine import Pin
 
 # this routine isn't nessecary - see bytes.hex(" ")
-# def format_bytes(bytestring):
-#     return " ".join("{:02x}".format(c) for c in bytestring)
+def format_bytes(bytestring):
+    return " ".join("{:02x}".format(c) for c in bytestring)
 
 
 def calculate_checksum(bytestring):
@@ -18,40 +18,50 @@ def calculate_checksum(bytestring):
 
 
 PIN_MAP = {
-   "MQTT": 14,
-   "D8"  : 12
-    }
+    "MQTT": 5,
+    "D8": 23,
+    "GSM": 19
+}
 
 
 # D8 is misleading, because the raw-PID is 0xD8, but the correct PID is "0x18"
 def set_led(s, b):
     p = Pin(PIN_MAP[s], Pin.OUT)
-    if b: p.value(0)
-    else: p.value(1)
-    
+    if b:
+        p.value(1)    #Status gedreht, da bei mir gegen Masse angeschlossen
+    else:
+        p.value(0)    #Status gedreht, da bei mir gegen Masse angeschlossen
+
+def get_led(s):
+    p = Pin(PIN_MAP[s], Pin.OUT)
+    return p.value()
+
 def toggle_led(s):
     p = Pin(PIN_MAP[s], Pin.OUT)
-    p.value(not(p.value()))
-    
+    p.value(not (p.value()))
+
+
 def dtoggle_led(s):
     p = Pin(PIN_MAP[s], Pin.OUT)
-    p.value(not(p.value()))
-    p.value(not(p.value()))
- 
-# pin, inverted
+    p.value(not (p.value()))  # ??? What is this function supposed to do? Two times "NOT" results in "YES", which does not change the LED status!
+    p.value(not (p.value()))
+
+
+
 def get_gpio(p, i):
     if i:
         p0 = Pin(p, Pin.IN, Pin.PULL_UP)
     else:
         p0 = Pin(p, Pin.IN, Pin.PULL_DOWN)
-    v = (p0.value() != i)    
+    v = (p0.value() != i)
 #    print("check pin", p, " inv: ", i, " Val: ", v)
     return v
 
 # pin, inverted, value ("ON", "OFF")
+
+
 def set_gpio(p, i, v):
     p0 = Pin(p, Pin.OUT)
     v = (v != i)
     p0.value(v)
 #    print("set pin", p, " inv: ", i, " to: ", v)
-
